84fabd39a80f51082e23f6194ab8eba6e611470d
kmcwharter <kristinmcwharter@gmail.com>
2022-05-10 10:09:22 -0500

    su

--- content/Su.md
+++ content/Su.md
@@ -2,7 +2,7 @@
 name: Su Jang
 title: ""
 position: 09
-url: "https://editor.p5js.org/sjang13/full/F15wVqdrh"
+url: "https://editor.p5js.org/sjang13/full/7focEYYcM"
 content_type: student
 color: "#c8c8ff"
 ---
---
+++ pages/about copy.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-between">
+    <h1 class="mx-auto mt-24">COMING SOON!</h1>
+    <nuxt-content :document="doc" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+  async asyncData({ $content, params }) {
+    const doc = await $content(params.slug).fetch();
+
+    return {
+      doc,
+    };
+  },
+};
+</script>
